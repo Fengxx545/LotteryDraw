7750e15e4ce6d725fe99c9fbe97cd098270a0284
Fengxx545 <569452415@qq.com>
2018-11-27 19:27:29 +0800

    抽奖转盘的实现

---
+++ app/src/main/java/com/ry/lotterydraw/view/LotterySurfaceView.java
@@ -0,0 +1,324 @@
+package com.ry.lotterydraw.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.ry.lotterydraw.R;
+
+/**
+ * Created by Hasee on 2018/11/27.
+ */
+
+public class LotterySurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private SurfaceHolder mHolder;
+    private Canvas mCanvas;
+    private Thread t;
+    private boolean isRuning;
+
+    /**
+     * 抽奖的文字
+     */
+    private String[] mStrs = new String[]{"圣诞树", "圣诞袜", "精美礼物", "闹钟",
+            "圣诞果", "精美礼物"};
+    /**
+     * 每个盘块的颜色
+     */
+    private int[] mColors = new int[]{0xFFFFC300, 0xFFF17E01, 0xFFFFC300,
+            0xFFF17E01, 0xFFFFC300, 0xFFF17E01};
+    /**
+     * 与文字对应的图片
+     */
+    private int[] mImgs = new int[]{R.drawable.christmas_tree, R.drawable.christmass_socks,
+            R.drawable.gif, R.drawable.clock, R.drawable.if_apple_2003315,
+            R.drawable.gif};
+
+    /**
+     * 与文字对应图片的bitmap数组
+     */
+    private Bitmap[] mImgsBitmap;
+    /**
+     * 盘块的个数
+     */
+    private int mItemCount = 6;
+
+    /**
+     * 绘制盘块的范围
+     */
+    private RectF mRange = new RectF();
+    /**
+     * 圆的直径
+     */
+    private int mRadius;
+    /**
+     * 绘制盘快的画笔
+     */
+    private Paint mArcPaint;
+
+    /**
+     * 绘制文字的画笔
+     */
+    private Paint mTextPaint;
+
+    /**
+     * 滚动的速度
+     */
+    private double mSpeed = 5;
+    private volatile float mStartAngle = 0;
+    /**
+     * 是否点击了停止
+     */
+    private boolean isShouldEnd;
+
+    /**
+     * 控件的中心位置
+     */
+    private int mCenter;
+    /**
+     * 控件的padding，这里我们认为4个padding的值一致，以paddingleft为标准
+     */
+    private int mPadding;
+
+    /**
+     * 背景图的bitmap
+     */
+    private Bitmap mBgBitmap = BitmapFactory.decodeResource(getResources(),
+            R.drawable.red_circle);
+    /**
+     * 文字的大小
+     */
+    private float mTextSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP, 20, getResources().getDisplayMetrics());
+
+
+    public LotterySurfaceView(Context context) {
+        this(context, null);
+    }
+
+    public LotterySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        setKeepScreenOn(true);
+
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+
+    }
+
+    /**
+     * 设置控件为正方形
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        // 获取圆形的直径
+        mRadius = width - getPaddingLeft() - getPaddingRight();
+        // padding值
+        mPadding = getPaddingLeft();
+        // 中心点
+        mCenter = width / 2;
+        setMeasuredDimension(width, width);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        // 初始化绘制圆弧的画笔
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setDither(true);
+        // 初始化绘制文字的画笔
+        mTextPaint = new Paint();
+        mTextPaint.setColor(0xFFffffff);
+        mTextPaint.setTextSize(mTextSize);
+        // 圆弧的绘制范围
+        mRange = new RectF(getPaddingLeft(), getPaddingLeft(), mRadius
+                + getPaddingLeft(), mRadius + getPaddingLeft());
+
+        // 初始化图片
+        mImgsBitmap = new Bitmap[mItemCount];
+        for (int i = 0; i < mItemCount; i++) {
+            mImgsBitmap[i] = BitmapFactory.decodeResource(getResources(),
+                    mImgs[i]);
+        }
+
+
+        isRuning = true;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        isRuning = false;
+    }
+
+    @Override
+    public void run() {
+        while (isRuning) {
+            long start = System.currentTimeMillis();
+            draw();
+            long end = System.currentTimeMillis();
+            if (end - start < 50) {
+                try {
+                    Thread.sleep(50 - (end - start));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void draw() {
+        try {
+            mCanvas = mHolder.lockCanvas();
+            if (mCanvas != null) {
+                //todo 进行绘制
+                //画背景
+                drawBg();
+
+                /**
+                 * 绘制每个块块，每个块块上的文本，每个块块上的图片
+                 */
+                float tmpAngle = mStartAngle;
+                float sweepAngle = (float) (360 / mItemCount);
+                for (int i = 0; i < mItemCount; i++) {
+                    // 绘制快快
+                    mArcPaint.setColor(mColors[i]);
+                    mCanvas.drawArc(mRange, tmpAngle, sweepAngle, true,
+                            mArcPaint);
+                    // 绘制文本
+                    drawText(tmpAngle, sweepAngle, mStrs[i]);
+                    // 绘制Icon
+                    drawIcon(tmpAngle, i);
+
+                    tmpAngle += sweepAngle;
+                }
+
+                // 如果mSpeed不等于0，则相当于在滚动
+                mStartAngle += mSpeed;
+
+                // 点击停止时，设置mSpeed为递减，为0值转盘停止
+                if (isShouldEnd) {
+                    mSpeed -= 1;
+                }
+                if (mSpeed <= 0) {
+                    mSpeed = 0;
+                    isShouldEnd = false;
+                }
+                // 根据当前旋转的mStartAngle计算当前滚动到的区域
+                calInExactArea(mStartAngle);
+
+
+            }
+        } catch (Exception e) {
+        } finally {
+            if (mCanvas != null) {
+                mHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+    }
+
+    private void drawIcon(float tmpAngle, int i) {
+        // 设置图片的宽度为直径的1/8
+        int imgWidth = mRadius / 8;
+
+        float angle = (float) ((30 + tmpAngle) * (Math.PI / 180));
+
+        int x = (int) (mCenter + mRadius / 2 / 2 * Math.cos(angle));
+        int y = (int) (mCenter + mRadius / 2 / 2 * Math.sin(angle));
+
+        // 确定绘制图片的位置
+        Rect rect = new Rect(x - imgWidth / 2, y - imgWidth / 2, x + imgWidth
+                / 2, y + imgWidth / 2);
+
+        mCanvas.drawBitmap(mImgsBitmap[i], null, rect, null);
+    }
+
+    private void drawText(float tmpAngle, float sweepAngle, String str) {
+        Path path = new Path();
+        path.addArc(mRange, tmpAngle, sweepAngle);
+        float textWidth = mTextPaint.measureText(str);
+        // 利用水平偏移让文字居中
+        float hOffset = (float) (mRadius * Math.PI / mItemCount / 2 - textWidth / 2);// 水平偏移
+        float vOffset = mRadius / 2 / 6;// 垂直偏移
+        mCanvas.drawTextOnPath(str, path, hOffset, vOffset, mTextPaint);
+    }
+
+    private void calInExactArea(float startAngle) {
+
+    }
+
+    /**
+     * 根据当前旋转的mStartAngle计算当前滚动到的区域 绘制背景，不重要，完全为了美观
+     */
+    private void drawBg() {
+        mCanvas.drawColor(0xFFFFFFFF);
+        mCanvas.drawBitmap(mBgBitmap, null, new Rect(mPadding / 2,
+                mPadding / 2, getMeasuredWidth() - mPadding / 2,
+                getMeasuredWidth() - mPadding / 2), null);
+    }
+
+
+    /**
+     * 点击开始旋转
+     *
+     * @param luckyIndex
+     */
+    public void start(int luckyIndex) {
+        // 每项角度大小
+        float angle = (float) (360 / mItemCount);
+        // 中奖角度范围（因为指针向上，所以水平第一项旋转到指针指向，需要旋转210-270；）
+        float from = 270 - (luckyIndex + 1) * angle;
+        float to = from + angle;
+        // 停下来时旋转的距离
+        float targetFrom = 4 * 360 + from;
+        /**
+         * <pre>
+         *  (v1 + 0) * (v1+1) / 2 = target ;
+         *  v1*v1 + v1 - 2target = 0 ;
+         *  v1=-1+(1*1 + 8 *1 * target)/2;
+         * </pre>
+         */
+        float v1 = (float) (Math.sqrt(1 * 1 + 8 * 1 * targetFrom) - 1) / 2;
+        float targetTo = 4 * 360 + to;
+        float v2 = (float) (Math.sqrt(1 * 1 + 8 * 1 * targetTo) - 1) / 2;
+
+        mSpeed = (float) (v1 + Math.random() * (v2 - v1));
+        isShouldEnd = false;
+    }
+
+    public void end() {
+        mStartAngle = 0;
+        isShouldEnd = true;
+    }
+
+    public boolean isStart(){
+        return mSpeed != 0;
+    }
+
+    public boolean isShouldEnd(){
+        return isShouldEnd;
+    }
+
+
+}
